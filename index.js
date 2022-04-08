90b9d26e58bff726788d3cbba53c52adf5f7d487
TIOGOUNG <+>
2022-04-08 17:59:56 -0400

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+
+const employee={name:"Sam",streetAddress:"canter court"}
+function updateEmployeeWithKeyAndValue (Employee,Key,Value){
+    const Sam={...Employee}
+    Sam[Key]=Value
+
+
+
+
+
+    return  Sam
+}
+const Employee={name:"Sam",streetAddress:"11 Broadway"}
+function destructivelyUpdateEmployeeWithKeyAndValue (Employee,key,value){
+  Employee[key]=value  
+    return Employee
+}
+function destructivelyDeleteFromEmployeeByKey (Employee,Key) {
+   delete Employee[Key]
+   return Employee
+}
+function deleteFromEmployeeByKey (Employee, key) {
+
+const Marc = {...Employee } 
+delete Marc[key]
+return Marc 
+}
+
